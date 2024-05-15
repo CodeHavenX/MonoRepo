83071aca3fe88260348f2e9fd23a79572caf0081
Cesar Ramirez <contact+gh@cramsan.com>
2024-05-15 13:52:20 -0700

    Deleted wrongly merged files

--- edifikana/front-end/app-android/src/androidMain/kotlin/com/cramsan/edifikana/client/android/screens/clockinout/add/ClockInOutSingleEmployeeUIEvent.kt
+++
@@ -1,7 +0,0 @@
-package com.cramsan.edifikana.client.android.screens.clockinout.add
-
-sealed class ClockInOutSingleEmployeeUIEvent {
-    data object Noop : ClockInOutSingleEmployeeUIEvent()
-
-    data object UploadCompleted : ClockInOutSingleEmployeeUIEvent()
-}

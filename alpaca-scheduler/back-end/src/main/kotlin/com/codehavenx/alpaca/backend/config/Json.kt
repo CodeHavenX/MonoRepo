3c73ff63ccc7481e6d364814d628ae9cf4c14346
gaaliciA1990 <garcia.alicia1990@gmail.com>
2024-08-19 21:04:02 -0700

    [Alpacal] Fixed other found issues with injection and DB access resolves 62

--- alpaca-scheduler/back-end/src/main/kotlin/com/codehavenx/alpaca/backend/config/Json.kt
+++ alpaca-scheduler/back-end/src/main/kotlin/com/codehavenx/alpaca/backend/config/Json.kt
@@ -6,4 +6,6 @@ import kotlinx.serialization.json.Json
  * Create a [Json] instance. This function is expected to be used as the single-source-of-truth for how should
  * json be serialized in this project.
  */
-fun createJson() = Json
+fun createJson() = Json {
+    prettyPrint = true
+}

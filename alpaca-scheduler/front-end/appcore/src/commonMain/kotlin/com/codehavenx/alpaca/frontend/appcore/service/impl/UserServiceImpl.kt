9be206f3559b0105fd642ea45b19845c049fee3e
cramsan <contact@cramsan.com>
2024-09-01 22:19:35 -0700

    [ALPACA] Cleaning up mappers

--- alpaca-scheduler/front-end/appcore/src/commonMain/kotlin/com/codehavenx/alpaca/frontend/appcore/service/impl/UserServiceImpl.kt
+++ alpaca-scheduler/front-end/appcore/src/commonMain/kotlin/com/codehavenx/alpaca/frontend/appcore/service/impl/UserServiceImpl.kt
@@ -3,6 +3,15 @@ package com.codehavenx.alpaca.frontend.appcore.service.impl
 import com.codehavenx.alpaca.frontend.appcore.models.User
 import com.codehavenx.alpaca.frontend.appcore.service.UserService
 import com.codehavenx.alpaca.frontend.appcore.utils.runSuspendCatching
+import com.codehavenx.alpaca.shared.api.Routes
+import com.codehavenx.alpaca.shared.api.annotations.NetworkModel
+import com.codehavenx.alpaca.shared.api.model.CreateUserRequest
+import com.codehavenx.alpaca.shared.api.model.UserResponse
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
 
 /**
  * Default implementation of the user service.
@@ -11,6 +20,14 @@ class UserServiceImpl : UserService {
 
     override suspend fun getUsers(): Result<List<User>> = runSuspendCatching(TAG) {
         listOf()
+    }
+
+    @OptIn(NetworkModel::class)
+    override suspend fun createUser(userName: String, email: String?, phoneNumber: String?) {
+        val request = CreateUserRequest.create(userName, email, phoneNumber)
+        httpClient.post(Routes.User.PATH) {
+            setBody(request)
+        }.body<UserResponse>().toModel()
     }
 
     companion object {

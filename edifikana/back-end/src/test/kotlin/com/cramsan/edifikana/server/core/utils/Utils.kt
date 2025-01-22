698074a1a776201e9525dfc707e2dc107319f0fb
gaaliciA1990 <garcia.alicia1990@gmail.com>
2025-01-21 19:15:13 -0500

    83 added string util to fixe crlf to lf for all BE tests.

--- edifikana/back-end/src/test/kotlin/com/cramsan/edifikana/server/core/utils/Utils.kt
+++ edifikana/back-end/src/test/kotlin/com/cramsan/edifikana/server/core/utils/Utils.kt
@@ -1,6 +1,9 @@
 package com.cramsan.edifikana.server.core.utils
 
+import com.cramsan.framework.utils.format.replaceWindowLEWithUnixLE
+
 /**
  * Helper function for reading a file from the resources directory.
  */
-fun Any.readFileContent(fileName: String) = this::class.java.classLoader.getResource(fileName)?.readText()
+fun Any.readFileContent(fileName: String) =
+    this::class.java.classLoader.getResource(fileName)?.readText()?.replaceWindowLEWithUnixLE()

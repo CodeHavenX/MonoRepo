7e803336605d6d988c6edac642f2ecacfdb95b9f
Cesar Ramirez <cesar.ramirez@airbnb.com>
2025-04-10 22:40:55 -0400

    [00][EDIFIKANA] Refactoring client Ktor DI

--- edifikana/front-end/shared-app/src/androidMain/kotlin/com/cramsan/edifikana/client/lib/di/koin/ExtrasPlatformModule.android.kt
+++ edifikana/front-end/shared-app/src/androidMain/kotlin/com/cramsan/edifikana/client/lib/di/koin/ExtrasPlatformModule.android.kt
@@ -1,32 +1,10 @@
 package com.cramsan.edifikana.client.lib.di.koin
 
-import com.cramsan.edifikana.client.lib.service.impl.AuthRequestPlugin
-import com.cramsan.edifikana.client.lib.settings.Overrides
-import com.cramsan.edifikana.lib.serialization.createJson
-import io.ktor.client.HttpClient
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.plugins.defaultRequest
-import io.ktor.serialization.kotlinx.json.json
 import org.koin.android.ext.koin.androidContext
-import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 actual val ExtrasPlatformModule = module {
 
     single { androidContext().contentResolver }
 
-    single {
-        HttpClient {
-            defaultRequest {
-                url("http://10.0.2.2:9292")
-            }
-            install(ContentNegotiation) {
-                json(createJson())
-            }
-            val disableSupabase = get<Boolean>(named(Overrides.KEY_DISABLE_SUPABASE))
-            if (!disableSupabase) {
-                install(AuthRequestPlugin(get()))
-            }
-        }
-    }
 }

9a1b6b3440cf71e2b8e55eef87e08ec9d350854b
cramsan <contact@cramsan.com>
2025-05-26 10:38:02 -0700

    [FRAMEWORK-SAMPLES] Adding a helth check to the ktor service sample

--- samples/service-ktor/src/main/kotlin/com/cramsan/samples/service/ktor/Application.kt
+++ samples/service-ktor/src/main/kotlin/com/cramsan/samples/service/ktor/Application.kt
@@ -7,13 +7,13 @@ import com.cramsan.samples.service.ktor.di.createKtorModule
 import io.ktor.http.HttpStatusCode
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.Application
-import io.ktor.server.application.call
 import io.ktor.server.application.install
 import io.ktor.server.plugins.calllogging.CallLogging
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.server.response.respond
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
+import io.ktor.server.routing.head
 import io.ktor.server.routing.routing
 import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.json.Json
@@ -75,7 +75,7 @@ fun Application.configureEntryPoints() {
         get("/") {
             call.respondText("Hello, World!")
         }
-        get("/health") {
+        head("/") {
             call.respond(HttpStatusCode.OK, "OK")
         }
     }

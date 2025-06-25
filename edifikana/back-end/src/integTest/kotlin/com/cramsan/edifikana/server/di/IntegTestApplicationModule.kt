f5ac08763bd048f36044aedf39c613b1abed77d9
Cesar Ramirez <contact+gh@cramsan.com>
2025-06-25 14:06:03 -0700

    [EDIFIKANA] Overwritting the config setting in integ tests (#182)

--- edifikana/back-end/src/integTest/kotlin/com/cramsan/edifikana/server/di/IntegTestApplicationModule.kt
+++ edifikana/back-end/src/integTest/kotlin/com/cramsan/edifikana/server/di/IntegTestApplicationModule.kt
@@ -1,5 +1,7 @@
 package com.cramsan.edifikana.server.di
 
+import com.cramsan.framework.configuration.Configuration
+import com.cramsan.framework.configuration.NoopConfiguration
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
@@ -7,6 +9,10 @@ import org.koin.dsl.module
  * Class to initialize the integ test pplication level components.
  */
 val IntegTestApplicationModule = module {
+    single<Configuration> {
+        NoopConfiguration()
+    }
+
     single<String>(named(STAGE_KEY)) {
         "integ"
     }

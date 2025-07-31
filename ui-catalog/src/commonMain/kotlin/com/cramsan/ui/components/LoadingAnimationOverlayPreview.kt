7fdcc63a0011bff8637b9c3a5201ec67bc3ebeb5
CRamsan <me@cramsan.com>
2025-07-31 08:05:47 -0700

    Fixing composable that could not be captured

--- ui-catalog/src/commonMain/kotlin/com/cramsan/ui/components/LoadingAnimationOverlayPreview.kt
+++ ui-catalog/src/commonMain/kotlin/com/cramsan/ui/components/LoadingAnimationOverlayPreview.kt
@@ -1,16 +1,28 @@
 package com.cramsan.ui.components
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Preview
 @Composable
 fun LoadingAnimationOverlayPreview_Loading() {
-    LoadingAnimationOverlay(isLoading = true)
+    Box(
+        modifier = Modifier.size(100.dp)
+    ) {
+        LoadingAnimationOverlay(isLoading = true)
+    }
 }
 
 @Preview
 @Composable
 fun LoadingAnimationOverlayPreview_NotLoading() {
-    LoadingAnimationOverlay(isLoading = false)
+    Box(
+        modifier = Modifier.size(100.dp)
+    ) {
+        LoadingAnimationOverlay(isLoading = false)
+    }
 }

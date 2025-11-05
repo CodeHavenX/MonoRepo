318c0cc5f19c95e703fd02f7c3c8e98aeb78dd77
Cesar Ramirez <contact@cramsan.com>
2025-11-04 23:08:12 -0800

    [DEPLOY] Enabling multi architecture

--- scripts/deploy_images.sh
+++ scripts/deploy_images.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
+set -e
 
-docker build -t ghcr.io/codehavenx/edifikana-backend:latest edifikana/back-end
-docker push ghcr.io/codehavenx/edifikana-backend:latest
+docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/codehavenx/edifikana-backend:latest edifikana/back-end/ --push
 
-docker build -t ghcr.io/codehavenx/edifikana-frontend:latest edifikana/front-end/app-wasm
-docker push ghcr.io/codehavenx/edifikana-frontend:latest
+docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/codehavenx/edifikana-frontend:latest edifikana/front-end/app-wasm --push
 
-docker build -t ghcr.io/codehavenx/runasimi-frontend:latest runasimi/front-end/app-wasm
-docker push ghcr.io/codehavenx/runasimi-frontend:latest
+docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/codehavenx/runasimi-frontend:latest runasimi/front-end/app-wasm --push
 
-docker build -t ghcr.io/codehavenx/samples-ktor-service:latest samples/service-ktor
-docker push ghcr.io/codehavenx/samples-ktor-service:latest
+docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/codehavenx/samples-ktor-service:latest samples/service-ktor --push
 
-docker build -t ghcr.io/codehavenx/samples-wasm-frontend:latest samples/jbcompose-wasm-app
-docker push ghcr.io/codehavenx/samples-wasm-frontend:latest
+docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/codehavenx/samples-wasm-frontend:latest samples/jbcompose-wasm-app --push

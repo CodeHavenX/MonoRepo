a1e9f40681a054c965ed2525395af8fa5394b633
Cesar Ramirez <contact@cramsan.com>
2025-12-04 13:30:51 -0800

    Adding AddPropertyScreen
    Addressing feedback
    
    #317 Adding a properties list screen
    
    Adding stuff
    
    # Conflicts:
    #	edifikana/front-end/shared-app/src/commonMain/kotlin/com/cramsan/edifikana/client/lib/features/home/organizationhome/OrganizationHomeScreen.kt
    #	edifikana/front-end/shared-app/src/commonMain/kotlin/com/cramsan/edifikana/client/lib/features/home/propertiesoverview/PropertiesOverviewEvent.kt
    #	edifikana/front-end/shared-app/src/commonMain/kotlin/com/cramsan/edifikana/client/lib/features/home/propertiesoverview/PropertiesOverviewScreen.kt
    #	edifikana/front-end/shared-app/src/commonMain/kotlin/com/cramsan/edifikana/client/lib/features/home/propertiesoverview/PropertiesOverviewScreen.preview.kt
    #	edifikana/front-end/shared-app/src/commonMain/kotlin/com/cramsan/edifikana/client/lib/features/home/propertiesoverview/PropertiesOverviewUIState.kt
    #	edifikana/front-end/shared-app/src/commonMain/kotlin/com/cramsan/edifikana/client/lib/features/home/propertiesoverview/PropertiesOverviewViewModel.kt
    #	edifikana/front-end/shared-app/src/jvmTest/kotlin/com/cramsan/edifikana/client/lib/features/home/propertiesoverview/PropertiesOverviewViewModelTest.kt
    
    # Conflicts:
    #	edifikana/front-end/shared-app/src/commonMain/kotlin/com/cramsan/edifikana/client/lib/features/home/addproperty/AddPropertyScreen.kt

--- edifikana/front-end/shared-app/src/commonMain/kotlin/com/cramsan/edifikana/client/lib/features/home/addproperty/AddPropertyScreen.kt
+++ edifikana/front-end/shared-app/src/commonMain/kotlin/com/cramsan/edifikana/client/lib/features/home/addproperty/AddPropertyScreen.kt
@@ -1,7 +1,9 @@
 package com.cramsan.edifikana.client.lib.features.home.addproperty
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -12,6 +14,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.LifecycleEventEffect
 import com.cramsan.edifikana.client.lib.features.home.HomeDestination

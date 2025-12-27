e89806cdc44e0cdf1c875508c9d4a816bba45922
Cesar Ramirez <cesar.ramirez@airbnb.com>
2025-12-27 03:42:32 -0800

    Create PropertyDetailScreenPreview.kt
    Adding missing preview file

---
+++ edifikana/front-end/shared-app/src/commonMain/kotlin/com/cramsan/edifikana/client/lib/features/home/propertydetail/PropertyDetailScreenPreview.kt
@@ -0,0 +1,54 @@
+package com.cramsan.edifikana.client.lib.features.home.propertydetail
+
+import androidx.compose.runtime.Composable
+import com.cramsan.edifikana.client.ui.theme.AppTheme
+import com.cramsan.edifikana.lib.model.PropertyId
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+/**
+ * Preview for the PropertyDetail feature screen.
+ */
+@Preview
+@Composable
+private fun PropertyDetailScreenPreview() = AppTheme {
+    PropertyDetailContent(
+        content = PropertyDetailUIState(
+            isLoading = false,
+            propertyId = PropertyId("preview-property-id"),
+            name = "Sample Property",
+            address = "123 Main Street, City, State 12345",
+            isEditMode = false,
+        ),
+        onBackSelected = {},
+        onEditSelected = {},
+        onCancelEdit = {},
+        onSaveProperty = {},
+        onDeleteProperty = {},
+        onNameChanged = {},
+        onAddressChanged = {},
+    )
+}
+
+/**
+ * Preview for the PropertyDetail feature screen in edit mode.
+ */
+@Preview
+@Composable
+private fun PropertyDetailScreenEditModePreview() = AppTheme {
+    PropertyDetailContent(
+        content = PropertyDetailUIState(
+            isLoading = false,
+            propertyId = PropertyId("preview-property-id"),
+            name = "Sample Property",
+            address = "123 Main Street, City, State 12345",
+            isEditMode = true,
+        ),
+        onBackSelected = {},
+        onEditSelected = {},
+        onCancelEdit = {},
+        onSaveProperty = {},
+        onDeleteProperty = {},
+        onNameChanged = {},
+        onAddressChanged = {},
+    )
+}

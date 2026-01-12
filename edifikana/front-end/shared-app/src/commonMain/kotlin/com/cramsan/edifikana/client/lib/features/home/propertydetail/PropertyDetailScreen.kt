9dc865cccba447b8ac3e432d0122409a5ff3f67a
gaalici1990 <garcia.alicia1990@proton.me>
2026-01-12 15:04:03 -0800

    detekt

--- edifikana/front-end/shared-app/src/commonMain/kotlin/com/cramsan/edifikana/client/lib/features/home/propertydetail/PropertyDetailScreen.kt
+++ edifikana/front-end/shared-app/src/commonMain/kotlin/com/cramsan/edifikana/client/lib/features/home/propertydetail/PropertyDetailScreen.kt
@@ -1,7 +1,6 @@
 package com.cramsan.edifikana.client.lib.features.home.propertydetail
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding

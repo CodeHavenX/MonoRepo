b3ed736ac991cffc98caa5c1bae41620522d7249
Cesar Ramirez <cesar.ramirez@airbnb.com>
2026-01-29 09:42:13 -0800

    [CI] Trying to fix the docker deploy step

--- scripts/deploy_images.sh
+++ scripts/deploy_images.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -e
 
-docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/codehavenx/edifikana-backend:latest edifikana/back-end/ --push
+export BUILDKIT_PROGRESS=plain
+export BUILDX_EXPERIMENTAL=1
 
-docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/codehavenx/edifikana-frontend:latest edifikana/front-end/app-wasm --push
+docker buildx build --progress=plain --platform linux/amd64,linux/arm64 -t ghcr.io/codehavenx/edifikana-backend:latest edifikana/back-end/ --push
 
-docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/codehavenx/runasimi-frontend:latest runasimi/front-end/app-wasm --push
+docker buildx build --progress=plain --platform linux/amd64,linux/arm64 -t ghcr.io/codehavenx/edifikana-frontend:latest edifikana/front-end/app-wasm --push
 
-docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/codehavenx/samples-ktor-service:latest samples/service-ktor --push
+docker buildx build --progress=plain --platform linux/amd64,linux/arm64 -t ghcr.io/codehavenx/runasimi-frontend:latest runasimi/front-end/app-wasm --push
 
-docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/codehavenx/samples-wasm-frontend:latest samples/jbcompose-wasm-app --push
+docker buildx build --progress=plain --platform linux/amd64,linux/arm64 -t ghcr.io/codehavenx/samples-ktor-service:latest samples/service-ktor --push
+
+docker buildx build --progress=plain --platform linux/amd64,linux/arm64 -t ghcr.io/codehavenx/samples-wasm-frontend:latest samples/jbcompose-wasm-app --push

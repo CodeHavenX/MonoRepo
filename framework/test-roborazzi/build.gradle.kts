6de7525123d318c30b4f6493e360e47ae3f70143
Cesar Ramirez <cesar.ramirez@airbnb.com>
2026-01-31 14:49:12 -0800

    [DEPS] Upgrading Roborazzi

--- framework/test-roborazzi/build.gradle.kts
+++ framework/test-roborazzi/build.gradle.kts
@@ -1,7 +1,13 @@
+import de.fayard.refreshVersions.core.versionFor
+
 plugins {
     kotlin("multiplatform")
     id("com.android.library")
 }
+
+// https://github.com/takahirom/roborazzi/issues/753
+// Due to an incompatibility with roborazzi, we cannot use the "_" notation from refreshVersions for this dependency
+val composablePreviewScannerVersion = versionFor("version.io.github.sergio-sastre.ComposablePreviewScanner..android")
 
 apply(from = "$rootDir/gradle/kotlin-mpp-target-common.gradle")
 apply(from = "$rootDir/gradle/kotlin-mpp-target-android-lib-compose.gradle")
@@ -17,7 +23,7 @@ dependencies {
     implementation("io.github.takahirom.roborazzi:roborazzi:_")
     implementation("io.github.takahirom.roborazzi:roborazzi-compose:_")
     implementation("io.github.takahirom.roborazzi:roborazzi-junit-rule:_")
-    implementation("io.github.sergio-sastre.ComposablePreviewScanner:android:_")
-    implementation("io.github.sergio-sastre.ComposablePreviewScanner:jvm:_")
+    implementation("io.github.sergio-sastre.ComposablePreviewScanner:android:$composablePreviewScannerVersion")
+    implementation("io.github.sergio-sastre.ComposablePreviewScanner:jvm:$composablePreviewScannerVersion")
     implementation("io.github.takahirom.roborazzi:roborazzi-compose-preview-scanner-support:_")
 }

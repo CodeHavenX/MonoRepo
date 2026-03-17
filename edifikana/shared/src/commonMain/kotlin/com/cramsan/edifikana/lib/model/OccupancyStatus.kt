75ad11448c657ec494435fc392d3492b36bfa7c1
gaalici1990 <garcia.alicia1990@proton.me>
2026-03-16 19:14:17 -0500

    [EDIFIKANA] #382 adding documentation for models

--- edifikana/shared/src/commonMain/kotlin/com/cramsan/edifikana/lib/model/OccupancyStatus.kt
+++ edifikana/shared/src/commonMain/kotlin/com/cramsan/edifikana/lib/model/OccupancyStatus.kt
@@ -1,5 +1,8 @@
 package com.cramsan.edifikana.lib.model
 
+/**
+ * Domain model representing the occupancy status of a unit occupant record.
+ */
 enum class OccupancyStatus {
     ACTIVE,
     INACTIVE,
--- edifikana/shared/src/commonMain/kotlin/com/cramsan/edifikana/lib/model/OccupantType.kt
+++ edifikana/shared/src/commonMain/kotlin/com/cramsan/edifikana/lib/model/OccupantType.kt
@@ -1,5 +1,8 @@
 package com.cramsan.edifikana.lib.model
 
+/**
+ * Domain model representing occupancy type of a unit occupant record.
+ */
 enum class OccupantType {
     TENANT,
     RESIDENT,

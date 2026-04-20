e57a7fdc30cd82a7885c29a2b65269d9904f4eb0
Cesar Ramirez <me@cramsan.com>
2026-04-20 02:39:13 -0700

    [AGENTIC] Removing more test code

--- flyerboard/front-end/shared-app/src/commonMain/kotlin/com/cramsan/flyerboard/client/lib/features/window/FlyerBoardWindowUIState.kt
+++
@@ -1,8 +0,0 @@
-package com.cramsan.flyerboard.client.lib.features.window
-
-import com.cramsan.framework.core.compose.ViewModelUIState
-
-/**
- * Window UI state.
- */
-data object FlyerBoardWindowUIState : ViewModelUIState

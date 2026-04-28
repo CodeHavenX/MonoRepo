97cda929f55e414ce8bd212f5b40031bb96aa04f
gaaliciA1990 <garcia.alicia1990@gmail.com>
2026-04-28 15:17:54 -0700

    [EDIFIKANA] #372 adding methods to controller

--- edifikana/back-end/src/main/kotlin/com/cramsan/edifikana/server/controller/StorageController.kt
+++ edifikana/back-end/src/main/kotlin/com/cramsan/edifikana/server/controller/StorageController.kt
@@ -1,20 +1,26 @@
 package com.cramsan.edifikana.server.controller
 
 import com.cramsan.edifikana.api.StorageApi
+import com.cramsan.edifikana.lib.model.asset.AssetId
 import com.cramsan.edifikana.lib.model.network.asset.AssetNetworkResponse
 import com.cramsan.edifikana.lib.model.network.asset.CreateAssetQueryParams
+import com.cramsan.edifikana.lib.model.network.asset.CreateSignedUploadQueryParams
+import com.cramsan.edifikana.lib.model.network.asset.GetAssetQueryParams
+import com.cramsan.edifikana.lib.model.network.asset.SignedUploadUrlNetworkResponse
 import com.cramsan.edifikana.lib.utils.requireNotBlank
 import com.cramsan.edifikana.server.controller.authentication.SupabaseContextPayload
 import com.cramsan.edifikana.server.service.StorageService
 import com.cramsan.framework.annotations.BackendController
 import com.cramsan.framework.annotations.api.BytesRequestBody
 import com.cramsan.framework.annotations.api.NoPathParam
+import com.cramsan.framework.annotations.api.NoRequestBody
 import com.cramsan.framework.core.ktor.Controller
 import com.cramsan.framework.core.ktor.OperationHandler.register
 import com.cramsan.framework.core.ktor.OperationRequest
 import com.cramsan.framework.core.ktor.auth.ClientContext
 import com.cramsan.framework.core.ktor.auth.ContextRetriever
 import com.cramsan.framework.core.ktor.handler
+import com.cramsan.framework.utils.exceptions.ClientRequestExceptions.NotFoundException
 import io.ktor.server.routing.Routing
 
 /**
@@ -50,12 +56,51 @@ class StorageController(
     }
 
     /**
+     * Handles retrieval of an asset by ID, returning a signed download URL.
+     */
+    suspend fun getAsset(
+        request: OperationRequest<
+            NoRequestBody,
+            GetAssetQueryParams,
+            NoPathParam,
+            ClientContext.AuthenticatedClientContext<SupabaseContextPayload>,
+            >,
+    ): AssetNetworkResponse {
+        val assetId = AssetId(requireNotBlank(request.queryParam.assetId))
+        val asset = storageService.getAsset(assetId)
+            ?: throw NotFoundException("Asset not found: $assetId")
+        return asset.toAssetNetworkResponse()
+    }
+
+    /**
+     * Handles creation of a signed upload URL for a given filename.
+     */
+    suspend fun createSignedUpload(
+        request: OperationRequest<
+            NoRequestBody,
+            CreateSignedUploadQueryParams,
+            NoPathParam,
+            ClientContext.AuthenticatedClientContext<SupabaseContextPayload>,
+            >,
+    ): SignedUploadUrlNetworkResponse {
+        val fileName = requireNotBlank(request.queryParam.filename)
+        val (signedUrl, path) = storageService.getSignedUploadUrl(fileName)
+        return SignedUploadUrlNetworkResponse(signedUrl = signedUrl, path = path)
+    }
+
+    /**
      * Registers the routes for the storage controller. The [route] parameter is the root path for the controller.
      */
     override fun registerRoutes(route: Routing) {
         StorageApi.register(route) {
             handler(api.createAsset, contextRetriever) { request ->
                 createAsset(request)
+            }
+            handler(api.getAsset, contextRetriever) { request ->
+                getAsset(request)
+            }
+            handler(api.createSignedUpload, contextRetriever) { request ->
+                createSignedUpload(request)
             }
         }
     }

81f8cbcb5bb0253c9c28823e310649562fda083f
Cesar Ramirez <me@cramsan.com>
2026-07-04 18:47:58 -0700

    [GRADLE] Updating intellij min version

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,7 +47,7 @@ ideCheck {
             minVersion.set("2026.1")
         }
         register("Idea") {
-            minVersion.set("2026.3")
+            minVersion.set("2026.1.4")
         }
     }
     // dirName must match the subdirectory name inside the IDE's plugins/ folder.

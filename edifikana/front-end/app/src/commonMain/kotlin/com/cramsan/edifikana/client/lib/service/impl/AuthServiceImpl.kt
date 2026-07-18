0af904b347dafa6f443552334f513fdd63548ba0
gaaliciA1990 <garcia.alicia1990@gmail.com>
2026-07-18 14:24:36 -0700

    [EDIFIKANA] detekt and openapi updates with releaseAll

--- edifikana/front-end/app/src/commonMain/kotlin/com/cramsan/edifikana/client/lib/service/impl/AuthServiceImpl.kt
+++ edifikana/front-end/app/src/commonMain/kotlin/com/cramsan/edifikana/client/lib/service/impl/AuthServiceImpl.kt
@@ -270,8 +270,7 @@ class AuthServiceImpl(private val auth: Auth, private val http: HttpClient) : Au
     override suspend fun notifyPasswordSet(): Result<Unit> =
         runSuspendCatching(TAG) {
             UserApi.setPasswordAuth
-                .buildRequest(
-                ).execute(http)
+                .buildRequest().execute(http)
         }
 
     override suspend fun inviteEmployee(
